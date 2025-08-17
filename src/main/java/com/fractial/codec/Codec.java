2a7a176eb4b3bdc761e44b93b29d7b57899e7b7c
Fractial <126729237+fractial@users.noreply.github.com>
2025-08-18 00:10:49 +0200

    Removed unused code

--- src/main/java/com/fractial/codec/Codec.java
+++ src/main/java/com/fractial/codec/Codec.java
@@ -11,5 +11,7 @@ public class Codec implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() { }
+	public void onInitialize() {
+
+	}
 }
